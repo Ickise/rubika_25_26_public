2de4b3c31535e6ac788709225f1fc78aabc55568
Enzo Dufour <enzo.dufour05@gmail.com>
2026-03-18 11:29:41 +0100

    Threshold 2 - Step 5

--- MyEngine_Correction/rubika_25_26/Engine/TaskMgr.cpp
+++ MyEngine_Correction/rubika_25_26/Engine/TaskMgr.cpp
@@ -11,7 +11,7 @@ void TaskMgr::Init()
 void TaskMgr::Shut()
 {
     {
-        std::unique_lock<std::mutex> guard(WorkerQueueMutex);
+        std::unique_lock<std::mutex> lock(WorkerQueueMutex);
         running = false;
     }
 
